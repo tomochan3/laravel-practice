be15c6fe0fcb225fea2a57bbc2404c661adbfd1e
tomocha3 <myoga.tomomitsu@gmail.com>
2020-05-19 00:17:54 +0900

    view

---
+++ src/resources/views/emails/customer.blade.php
@@ -0,0 +1,7 @@
+<!-- resources/views/greeting.blade.phpとして保存されているビュー -->
+
+<html>
+    <body>
+        <h1>Hello!!</h1>
+    </body>
+</html>
---
+++ src/resources/views/greeting.blade.php
@@ -0,0 +1,7 @@
+<!-- resources/views/greeting.blade.phpとして保存されているビュー -->
+
+<html>
+    <body>
+        <h1>Hello, {{ $name }}</h1>
+    </body>
+</html>
